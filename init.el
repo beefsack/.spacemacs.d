76ba54c32bb39dae15e8bc4673d816770f43a73c
Michael Alexander <beefsack@gmail.com>
2016-06-23 23:19:43 +1000

    Increased font size

--- init.el
+++ init.el
@@ -131,7 +131,7 @@ values."
    ;; Default font. `powerline-scale' allows to quickly tweak the mode-line
    ;; size to make separators look not too crappy.
    dotspacemacs-default-font '("Source Code Pro"
-                               :size 15
+                               :size 16
                                :weight medium
                                :width normal
                                :powerline-scale 1.1)
